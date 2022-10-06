c000461e89d06a7a054ce470221c1cc1adee26ae
Mari-Anne Eerikäinen <mari-anne.eerikainen@awacademy.training>
2022-10-06 10:53:58 +0300

    4.2

---
+++ blogilista/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('error connecting to MongoDB: ', error.message)
+  })
+
+app.use(cors())
+app.use(express())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
